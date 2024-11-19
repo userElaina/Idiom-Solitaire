6e7e63fbea748a12996eb839dc07252f7dd94cf5
userElaina <userelaina@pm.me>
2024-11-19 13:57:46 +0000

    :sparkles: utils

---
+++ utils.py
@@ -0,0 +1,12 @@
+import json
+
+ZH_PATTERN = r'[\u4e00-\u9fa5]+'
+
+
+def svj(j: dict, nm: str) -> None:
+    s = json.dumps(j, ensure_ascii=False, indent=4)
+    open('result/%s.json' % nm, 'wb').write(s.encode('utf8'))
+
+
+def rdj(nm: str) -> dict:
+    return json.loads(open('result/%s.json' % nm, 'rb').read().decode('utf8'))
