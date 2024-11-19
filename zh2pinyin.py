e2274c22bf854766c05637b034ade2dd9063fe47
userElaina <userelaina@pm.me>
2024-11-19 09:10:52 +0000

    :truck: spider

--- zh2pinyin.py
+++
@@ -1,6 +0,0 @@
-# pip install pypinyin
-
-from pypinyin import pinyin, Style
-
-print(pinyin(['aa你a v好☆dd☆','a','你的'], style=Style.NORMAL, heteronym=True, v_to_u=False))
-# [['aa'], ['ni'], ['a v'], ['hao'], ['☆dd☆'], ['a'], ['ni'], ['de', 'di']]
